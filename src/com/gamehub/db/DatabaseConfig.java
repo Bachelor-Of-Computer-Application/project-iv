662dc0de0f1cce8f440abd50cd3dd0a7ba18cd1b
Abinashkumal <abinashkumal45@gmail.com>
2026-06-19 00:49:47 +0545

    solve problem of db connection and font conflit to jdk

--- src/com/gamehub/db/DatabaseConfig.java
+++ src/com/gamehub/db/DatabaseConfig.java
@@ -3,7 +3,7 @@ package com.gamehub.db;
 public final class DatabaseConfig {
     public static final String URL = "jdbc:mysql://localhost:3306/game_hub?useSSL=false&serverTimezone=UTC";
     public static final String USER = "root";
-    public static final String PASSWORD = "";
+    public static final String PASSWORD = "root123";
 
     private DatabaseConfig() {
     }
